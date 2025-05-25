6c223915954f6495450b19c431b0da70fee34ed5
fronx <fronx@wurmus.de>
2025-05-25 21:38:35 +0200

    Deploy script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+npm install
+npm run build
+
+[ $? -eq 0 ] && echo "Build successful, continuing with deployment..." || { echo "Build failed, aborting deployment"; exit 1; }
+
+OBSIDIAN_VAULT_PATH="$HOME/Documents/Obsidian Vault"
+cp main.js manifest.json "$OBSIDIAN_VAULT_PATH/.obsidian/plugins/llm-workspace/"
+
+[ $? -eq 0 ] && echo "Done"
+
